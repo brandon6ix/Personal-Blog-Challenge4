9c75e0dcd626599a2ba6702395844c4bd393220d
Brandon Willis <brandonwillis18@icloud.com>
2024-06-08 18:54:13 -0400

    Added dark and light mode to blog page

--- assets/js/logic.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const modeToggle = document.getElementById('modeToggle');
-
-    modeToggle.addEventListener('change', function() {
-        document.body.classList.toggle('dark-mode');
-    });
-});
